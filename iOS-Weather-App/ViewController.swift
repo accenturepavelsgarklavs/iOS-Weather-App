ef13aa5291df9c26cf11d07a5ad1ee074a74dbdd
Pāvels Garklāvs <pavels.garklavs@accenture.com>
2022-02-22 10:22:04 +0200

    Initial Commit

---
+++ iOS-Weather-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Weather-App
+//
+//  Created by pavels.garklavs on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
